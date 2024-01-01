09c0e1f5ea3917ef3a718a4b95528f473ce988bf
Rafael Souza <rafa.nds13@gmail.com>
2024-01-01 20:31:37 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/rafael/springbootmondodb/SpringbootMondodbApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.springbootmondodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMondodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMondodbApplication.class, args);
+	}
+
+}
