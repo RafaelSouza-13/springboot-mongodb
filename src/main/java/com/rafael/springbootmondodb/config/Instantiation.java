cf1906179e6d7c94c4265087da56b2010bfa1112
Rafael Souza <rafa.nds13@gmail.com>
2024-01-03 08:45:21 -0300

    Adicionando a classe usuario as referencias de pots

--- src/main/java/com/rafael/springbootmondodb/config/Instantiation.java
+++ src/main/java/com/rafael/springbootmondodb/config/Instantiation.java
@@ -33,11 +33,14 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
         userRepositorie.saveAll(Arrays.asList(maria, alex, bob));
-        
+
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Adeus pessoal, irei viajar", new AuthorDto(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei bem leizs", new AuthorDto(maria));
 
         postRepositorie.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepositorie.save(maria);
 
 
     }
