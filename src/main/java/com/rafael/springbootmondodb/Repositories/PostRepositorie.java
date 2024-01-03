829a4ec93359108ab106bc6becb75d00be938b32
Rafael Souza <rafa.nds13@gmail.com>
2024-01-03 09:45:00 -0300

    Consulta simples com @query

--- src/main/java/com/rafael/springbootmondodb/Repositories/PostRepositorie.java
+++ src/main/java/com/rafael/springbootmondodb/Repositories/PostRepositorie.java
@@ -3,6 +3,7 @@ package com.rafael.springbootmondodb.Repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.rafael.springbootmondodb.domain.Post;
@@ -10,4 +11,7 @@ import com.rafael.springbootmondodb.domain.Post;
 @Repository
 public interface PostRepositorie extends MongoRepository<Post, String> {
     public List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    public List<Post> searchTitle(String text);
 }
